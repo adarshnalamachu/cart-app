5ffea2e9acb7fce571fb393b325475106fbb09dc
adarshnalamachu <adarsh.n@mind-graph.com>
2025-12-16 22:32:51 +0530

    state holding on refresh

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,31 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartSlice from "./slices/cartSlice.js";
-//create a store and give it reducers
-const store = configureStore(
-  { reducer: 
-    { cart: cartSlice } });
+import cartReducer from "./slices/cartSlice";
+import storage from "redux-persist/lib/storage"; // localStorage
+import { persistReducer, persistStore } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+// Persist config
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// Combine reducers
+const rootReducer = combineReducers({
+  cart: cartReducer,
+});
+
+// Persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // REQUIRED for redux-persist
+    }),
+});
+
+export const persistor = persistStore(store);
+
 export default store;
